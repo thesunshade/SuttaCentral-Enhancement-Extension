e55cd33228297a3f60272e6203403ef052a9fa74
thesunshade <82448383+thesunshade@users.noreply.github.com>
2024-10-03 14:11:41 +1300

    root first, parallels, other trans

--- entrypoints/colorOverrides.content.ts
+++ entrypoints/colorOverrides.content.ts
@@ -9,27 +9,49 @@ export default defineContentScript({
     const styleTag = document.createElement("style");
 
     styleTag.textContent = `
+
 body{
---sc-primary-color: #9f05c6!important;
-  --sc-primary-color-light: #ca10f9!important;
-  --sc-primary-color-light-transparent: #ca10f966!important;
-  --sc-primary-color-dark: hsl(288 95% 32% / 1)!important;
-  --sc-primary-accent-color: #c18b49!important;
-  --sc-primary-accent-color-light: hsl(33 85% 65% / 1)!important;
-  --sc-secondary-accent-color: #C03030!important;
-  --sc-secondary-accent-color-light: #CC5959!important;
-  --sc-primary-background-color: #fdf5ff!important;
-  --sc-on-primary-primary-text-color: #201b13!important;
-  --sc-on-primary-secondary-text-color: #7c766f!important;
-  --sc-secondary-background-color: #fffbff!important;
-  --sc-on-secondary-primary-text-color: #211400!important;
-  --sc-on-secondary-secondary-text-color: #807567!important;
-  --sc-tertiary-background-color: hsl(288 65% 92% / 1)!important;
+--sc-primary-color: #c60505!important;
+--sc-primary-color-light: #f91010!important;
+--sc-primary-color-light-transparent: #f9101066!important;
+--sc-primary-color-dark: #9f0404!important;
+--sc-primary-accent-color: #ffba63!important;
+--sc-primary-accent-color-light: #ffd39c!important;
+--sc-secondary-accent-color: #C03030!important;
+--sc-secondary-accent-color-light: #CC5959!important;
+--sc-primary-background-color: #fff5f5!important;
+--sc-on-primary-primary-text-color: #201b13!important;
+--sc-on-primary-secondary-text-color: #7c6e6e!important;
+--sc-secondary-background-color: #fffbff!important;
+--sc-on-secondary-primary-text-color: #211400!important;
+--sc-on-secondary-secondary-text-color: #807567!important;
+--sc-tertiary-background-color: #f8dddd!important;
+--sc-on-tertiary-primary-text-color: #00192f!important;
+--sc-on-tertiary-secondary-text-color: #675d4f!important;
+--sc-dark-fixed-background-color: #544f4f!important;
+--sc-darker-fixed-background-color: #403c3c!important;
 }
 `;
     document.head.appendChild(styleTag);
   },
 });
+
+// Grimace
+// --sc-primary-color: #9f05c6!important;
+//   --sc-primary-color-light: #ca10f9!important;
+//   --sc-primary-color-light-transparent: #ca10f966!important;
+//   --sc-primary-color-dark: hsl(288 95% 32% / 1)!important;
+//   --sc-primary-accent-color: #c18b49!important;
+//   --sc-primary-accent-color-light: hsl(33 85% 65% / 1)!important;
+//   --sc-secondary-accent-color: #C03030!important;
+//   --sc-secondary-accent-color-light: #CC5959!important;
+//   --sc-primary-background-color: #fdf5ff!important;
+//   --sc-on-primary-primary-text-color: #201b13!important;
+//   --sc-on-primary-secondary-text-color: #7c766f!important;
+//   --sc-secondary-background-color: #fffbff!important;
+//   --sc-on-secondary-primary-text-color: #211400!important;
+//   --sc-on-secondary-secondary-text-color: #807567!important;
+//   --sc-tertiary-background-color: hsl(288 65% 92% / 1)!important;
 
 // Shamrock shake
 // body{
@@ -49,86 +71,178 @@ body{
 // }
 
 // birdie
-// --sc-primary-color: #c60577;
-// --sc-primary-color-light: #f9109c;
-// --sc-primary-color-light-transparent: #f9109c66;
-// --sc-primary-color-dark: hsl(324 95% 32% / 1);
-// --sc-primary-accent-color: #48c1b1;
-// --sc-primary-accent-color-light: #5af2dd;
-// --sc-secondary-accent-color: #C03030;
-// --sc-secondary-accent-color-light: #CC5959;
-// --sc-primary-background-color: #fff5fb;
-// --sc-on-primary-primary-text-color: #201b13;
-// --sc-on-primary-secondary-text-color: #7c766f;
-// --sc-secondary-background-color: hsl(0deg 0% 99.22%);
-// --sc-on-secondary-primary-text-color: #211400;
-// --sc-on-secondary-secondary-text-color: #807567;
-// --sc-tertiary-background-color: #f8dded;
-// --sc-on-tertiary-primary-text-color: #00192f;
-// --sc-on-tertiary-secondary-text-color: #675d4f;
-// --sc-dark-fixed-background-color: #544f4f;
-// --sc-darker-fixed-background-color: #3a3036;
+// --sc-primary-color: #c60577!important;
+// --sc-primary-color-light: #f9109c!important;
+// --sc-primary-color-light-transparent: #f9109c66!important;
+// --sc-primary-color-dark: hsl(324 95% 32% / 1)!important;
+// --sc-primary-accent-color: #48c1b1!important;
+// --sc-primary-accent-color-light: #5af2dd!important;
+// --sc-secondary-accent-color: #C03030!important;
+// --sc-secondary-accent-color-light: #CC5959!important;
+// --sc-primary-background-color: #fff5fb!important;
+// --sc-on-primary-primary-text-color: #201b13!important;
+// --sc-on-primary-secondary-text-color: #7c766f!important;
+// --sc-secondary-background-color: hsl(0deg 0% 99.22%)!important;
+// --sc-on-secondary-primary-text-color: #211400!important;
+// --sc-on-secondary-secondary-text-color: #807567!important;
+// --sc-tertiary-background-color: #f8dded!important;
+// --sc-on-tertiary-primary-text-color: #00192f!important;
+// --sc-on-tertiary-secondary-text-color: #675d4f!important;
+// --sc-dark-fixed-background-color: #544f4f!important;
+// --sc-darker-fixed-background-color: #3a3036!important;
 
 // hamburgler
+// --scrollbar-width: 17px!important;
+// --sc-primary-color: #666!important;
+// --sc-primary-color-light: #858585!important;
+// --sc-primary-color-light-transparent: #85858566!important;
+// --sc-primary-color-dark: #525252!important;
+// --sc-primary-accent-color: #000!important;
+// --sc-primary-accent-color-light: #5aa8f2!important;
+// --sc-secondary-accent-color: #C03030!important;
+// --sc-secondary-accent-color-light: #CC5959!important;
+// --sc-primary-background-color: #fafafa!important;
+// --sc-on-primary-primary-text-color: #201b13!important;
+// --sc-on-primary-secondary-text-color: #7c766f!important;
+// --sc-secondary-background-color: #fffbff!important;
+// --sc-on-secondary-primary-text-color: #211400!important;
+// --sc-on-secondary-secondary-text-color: #807567!important;
+// --sc-tertiary-background-color: #ebebeb!important;
+// --sc-on-tertiary-primary-text-color: #00192f!important;
+// --sc-on-tertiary-secondary-text-color: #675d4f!important;
+// --sc-dark-fixed-background-color: #544f4f!important;
+// --sc-darker-fixed-background-color: #000000!important;
+
+// ronald
+// --sc-primary-color: #c60505!important;
+// --sc-primary-color-light: #f91010!important;
+// --sc-primary-color-light-transparent: #f9101066!important;
+// --sc-primary-color-dark: #9f0404!important;
+// --sc-primary-accent-color: #ffba63!important;
+// --sc-primary-accent-color-light: #ffd39c!important;
+// --sc-secondary-accent-color: #C03030!important;
+// --sc-secondary-accent-color-light: #CC5959!important;
+// --sc-primary-background-color: #fff5f5!important;
+// --sc-on-primary-primary-text-color: #201b13!important;
+// --sc-on-primary-secondary-text-color: #7c6e6e!important;
+// --sc-secondary-background-color: #fffbff!important;
+// --sc-on-secondary-primary-text-color: #211400!important;
+// --sc-on-secondary-secondary-text-color: #807567!important;
+// --sc-tertiary-background-color: #f8dddd!important;
+// --sc-on-tertiary-primary-text-color: #00192f!important;
+// --sc-on-tertiary-secondary-text-color: #675d4f!important;
+// --sc-dark-fixed-background-color: #544f4f!important;
+// --sc-darker-fixed-background-color: #403c3c!important;
+
+//OBM
+// --sc-primary-color: #0528c6!important;
+//   --sc-primary-color-light: #103bf9!important;
+//   --sc-primary-color-light-transparent: #103bf966!important;
+//   --sc-primary-color-dark: #04219f!important;
+//   --sc-primary-accent-color: #ffba63!important;
+//   --sc-primary-accent-color-light: #ffd49f!important;
+//   --sc-secondary-accent-color: #C03030!important;
+//   --sc-secondary-accent-color-light: #CC5959!important;
+//   --sc-primary-background-color: #f5f7ff!important;
+//   --sc-on-primary-primary-text-color: #201b13!important;
+//   --sc-on-primary-secondary-text-color: #7c766f!important;
+//   --sc-secondary-background-color: #fffbff!important;
+//   --sc-on-secondary-primary-text-color: #211400!important;
+//   --sc-on-secondary-secondary-text-color: #807567!important;
+//   --sc-tertiary-background-color: #dde2f8!important;
+//   --sc-on-tertiary-primary-text-color: #00192f!important;
+//   --sc-on-tertiary-secondary-text-color: #675d4f!important;
+//   --sc-dark-fixed-background-color: #544f4f!important;
+//   --sc-darker-fixed-background-color: #383b47!important;
+
+//----------------------------------
+
+// shamrock shake Dark
 // --scrollbar-width: 17px;
-// --sc-primary-color: #666;
-// --sc-primary-color-light: #858585;
-// --sc-primary-color-light-transparent: #85858566;
-// --sc-primary-color-dark: #525252;
-// --sc-primary-accent-color: #000;
+//     --sc-primary-color: hsl(120deg 99% 25.32%);
+//     --sc-primary-color-light: hsl(120deg 85.35% 37.22%);
+//     --sc-primary-color-light-transparent: hsl(120deg 94% 79% / 20%);
+//     --sc-primary-color-dark: hsl(120 95% 32% / 1);
+//     --sc-primary-accent-color: #5aa8f2;
+//     --sc-primary-accent-color-light: #4886C1;
+//     --sc-secondary-accent-color: #C03030;
+//     --sc-secondary-accent-color-light: #CC5959;
+//     --sc-primary-background-color: #414141;
+//     --sc-on-primary-primary-text-color: #eeeeee;
+//     --sc-on-primary-secondary-text-color: #cccccc;
+//     --sc-secondary-background-color: #515151;
+//     --sc-on-secondary-primary-text-color: #efefef;
+//     --sc-on-secondary-secondary-text-color: #cdcdcd;
+//     --sc-tertiary-background-color: #616161;
+//     --sc-on-tertiary-primary-text-color: #ffffff;
+//     --sc-on-tertiary-secondary-text-color: #dddddd;
+//     --sc-dark-fixed-background-color: #515151;
+//     --sc-darker-fixed-background-color: hsl(120deg 88.06% 6.36%);
+
+// =========================================================
+
+// bubblegum
+// --scrollbar-width: 17px;
+// --sc-primary-color: #05bdc6;
+// --sc-primary-color-light: #7bf9ff;
+// --sc-primary-color-light-transparent: rgb(15 243 249 / 40%);
+// --sc-primary-color-dark: #f1319a;
+// --sc-primary-accent-color: #4886C1;
 // --sc-primary-accent-color-light: #5aa8f2;
 // --sc-secondary-accent-color: #C03030;
 // --sc-secondary-accent-color-light: #CC5959;
-// --sc-primary-background-color: #fafafa;
+// --sc-primary-background-color: #f3ffff;
+// --sc-on-primary-primary-text-color: #201b13;
+// --sc-on-primary-secondary-text-color: #7c766f;
+// --sc-secondary-background-color: #ffeef8;
+// --sc-on-secondary-primary-text-color: #211400;
+// --sc-on-secondary-secondary-text-color: #807567;
+// --sc-tertiary-background-color: #def7f8;
+// --sc-on-tertiary-primary-text-color: #00192f;
+// --sc-on-tertiary-secondary-text-color: #675d4f;
+// --sc-dark-fixed-background-color: #544f4f;
+// --sc-darker-fixed-background-color: #003a3b;
+
+// sonora
+// --scrollbar-width: 17px;
+//     --sc-primary-color: #FF9800;
+//     --sc-primary-color-light: #B80000;
+//     --sc-primary-color-light-transparent: #5f86706e;
+//     --sc-primary-color-dark: #5F8670;
+//     --sc-primary-accent-color: #4886C1;
+//     --sc-primary-accent-color-light: #5aa8f2;
+//     --sc-secondary-accent-color: #C03030;
+//     --sc-secondary-accent-color-light: #CC5959;
+//     --sc-primary-background-color: #fff8f3;
+//     --sc-on-primary-primary-text-color: #201b13;
+//     --sc-on-primary-secondary-text-color: #7c766f;
+//     --sc-secondary-background-color: #fffbff;
+//     --sc-on-secondary-primary-text-color: #211400;
+//     --sc-on-secondary-secondary-text-color: #807567;
+//     --sc-tertiary-background-color: #d0e8db;
+//     --sc-on-tertiary-primary-text-color: #00192f;
+//     --sc-on-tertiary-secondary-text-color: #675d4f;
+//     --sc-dark-fixed-background-color: #544f4f;
+//     --sc-darker-fixed-background-color: #820300;
+
+// pumpkin spice
+// --scrollbar-width: 17px;
+// --sc-primary-color: #c65e05;
+// --sc-primary-color-light: hsl(28 95% 52% / 1);
+// --sc-primary-color-light-transparent: rgb(249 125 16 / 40%);
+// --sc-primary-color-dark: hwb(28deg 1.6% 37.6%);
+// --sc-primary-accent-color: #c40000;
+// --sc-primary-accent-color-light: #ff6e6e;
+// --sc-secondary-accent-color: #C03030;
+// --sc-secondary-accent-color-light: #CC5959;
+// --sc-primary-background-color: #fff8f3;
 // --sc-on-primary-primary-text-color: #201b13;
 // --sc-on-primary-secondary-text-color: #7c766f;
 // --sc-secondary-background-color: #fffbff;
 // --sc-on-secondary-primary-text-color: #211400;
 // --sc-on-secondary-secondary-text-color: #807567;
-// --sc-tertiary-background-color: #ebebeb;
+// --sc-tertiary-background-color: hsl(28 65% 92% / 1);
 // --sc-on-tertiary-primary-text-color: #00192f;
 // --sc-on-tertiary-secondary-text-color: #675d4f;
 // --sc-dark-fixed-background-color: #544f4f;
-// --sc-darker-fixed-background-color: #000000;
-
-// ronald
-// --sc-primary-color: #c60505;
-// --sc-primary-color-light: #f91010;
-// --sc-primary-color-light-transparent: #f9101066;
-// --sc-primary-color-dark: #9f0404;
-// --sc-primary-accent-color: #ffba63;
-// --sc-primary-accent-color-light: #ffd39c;
-// --sc-secondary-accent-color: #C03030;
-// --sc-secondary-accent-color-light: #CC5959;
-// --sc-primary-background-color: #fff5f5;
-// --sc-on-primary-primary-text-color: #201b13;
-// --sc-on-primary-secondary-text-color: #7c6e6e;
-// --sc-secondary-background-color: #fffbff;
-// --sc-on-secondary-primary-text-color: #211400;
-// --sc-on-secondary-secondary-text-color: #807567;
-// --sc-tertiary-background-color: #f8dddd;
-// --sc-on-tertiary-primary-text-color: #00192f;
-// --sc-on-tertiary-secondary-text-color: #675d4f;
-// --sc-dark-fixed-background-color: #544f4f;
-// --sc-darker-fixed-background-color: #403c3c;
-
-//OBM
-// --sc-primary-color: #0528c6;
-//   --sc-primary-color-light: #103bf9;
-//   --sc-primary-color-light-transparent: #103bf966;
-//   --sc-primary-color-dark: #04219f;
-//   --sc-primary-accent-color: #ffba63;
-//   --sc-primary-accent-color-light: #ffd49f;
-//   --sc-secondary-accent-color: #C03030;
-//   --sc-secondary-accent-color-light: #CC5959;
-//   --sc-primary-background-color: #f5f7ff;
-//   --sc-on-primary-primary-text-color: #201b13;
-//   --sc-on-primary-secondary-text-color: #7c766f;
-//   --sc-secondary-background-color: #fffbff;
-//   --sc-on-secondary-primary-text-color: #211400;
-//   --sc-on-secondary-secondary-text-color: #807567;
-//   --sc-tertiary-background-color: #dde2f8;
-//   --sc-on-tertiary-primary-text-color: #00192f;
-//   --sc-on-tertiary-secondary-text-color: #675d4f;
-//   --sc-dark-fixed-background-color: #544f4f;
-//   --sc-darker-fixed-background-color: #383b47;
+// --sc-darker-fixed-background-color: hsl(28deg 100% 9.29%);
