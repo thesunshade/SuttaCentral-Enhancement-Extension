f7e788774bcac729f9161d645000718043919188
thesunshade <82448383+thesunshade@users.noreply.github.com>
2024-10-15 10:56:16 +1300

    add instant lookup

---
+++ src/entrypoints/functions/normalizeString.ts
@@ -0,0 +1,7 @@
+export default function normalizeString(str) {
+  return str
+    .normalize("NFD") // Decompose diacritics
+    .replace(/[\u0300-\u036f]/g, "") // Remove diacritics
+    .replace(/[\s,;.“”'"’/()-]/g, "") // Remove spaces and punctuation
+    .toLowerCase();
+}
